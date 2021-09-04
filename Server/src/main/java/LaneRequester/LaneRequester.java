cbd8fb6116cea5dae1038f290e832029d9e45e0a
Ignacio <isampedro@itba.edu.ar>
2021-09-04 18:16:54 -0300

    Added lane requester class

---
+++ Server/src/main/java/LaneRequester/LaneRequester.java
@@ -0,0 +1,9 @@
+package LaneRequester;
+
+import Airport.Categories;
+
+public class LaneRequester {
+    public static void requestFlightLane(int flightId, int destinyAirport, String airline, Categories minimumCategory ) {
+        
+    }
+}
