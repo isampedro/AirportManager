b342cfdadac5d4bc2e7fa95bca5b2cf5d4b058eb
R0L02796 <alejandro.rolandelli@gmail.com>
2021-09-08 16:15:22 -0300

    server creation

--- server/src/main/java/ar/edu/itba/pod/rmi/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/rmi/server/Server.java
@@ -3,10 +3,30 @@ package ar.edu.itba.pod.rmi.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
+    private final static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
-        logger.info("rmi-params Server Starting ...");
+
+        logger.info("Airport Manager Server Starting ...");
+
+        final Servant servant = new Servant();
+        logger.info("Servant created");
+        try {
+            final Registry registry = LocateRegistry.getRegistry();
+            final Remote adminRemote = UnicastRemoteObject.exportObject(servant,0);
+            registry.rebind("service",adminRemote);
+        }catch (RemoteException e){
+            logger.error("Connection to registry failed");
+            logger.error(e.getMessage());
+        }
+        logger.info("Server is ready");
+
     }
 }
