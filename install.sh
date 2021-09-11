91d3dce2358aeecc0c7dcc1710c2c73fbe4cde99
agbossi <abossi@itba.edu.ar>
2021-09-11 17:14:49 -0300

    install.sh fix

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo "$PATH"
 tar -xzf *-bin.tar.gz
 cd rmi-params-server-1.0-SNAPSHOT
 chmod u+x run-*
-cd ../../client/target
+cd ../../../client/target
 echo "$PATH"
 tar -xzf *-bin.tar.gz
 cd rmi-params-client-1.0-SNAPSHOT
 chmod u+x run-*
-cd ../../
+cd ../../../
